311a28d7af79c09f7fc674fa8dc261d707b2d03f
alex.zakarov <talha.semerci394@gmail.com>
2025-05-24 21:20:53 +0300

    feat(dataset): add default policy

--- data/dataset.go
+++ data/dataset.go
@@ -58,3 +58,21 @@ func (d *dataSet) GetFeatureValues(candles []domain.Candle, feat_type domain.Fea
 	}
 	return
 }
+
+// ClosePercentagePolicy is a default policy
+func ClosePercentagePolicy(candles []domain.Candle) int {
+	perChange := 0.
+	for i, _ := range candles {
+		if i == 0 {
+			continue
+		}
+		perChange += ((candles[i].Open - candles[i-1].Open) / candles[i-1].Open) * 100
+	}
+	if perChange > 0.01 {
+		return 1
+	} else if perChange < 0.01 && perChange > -.01 {
+		return 0
+	} else {
+		return -1
+	}
+}
